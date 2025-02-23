daca4298a336d6a026d83eed1e4ea4594af507a1
Chris J <ch436608@ucf.edu>
2025-02-23 23:18:29 +0000

    update contact php

---
+++ public/api/Update.php
@@ -0,0 +1,55 @@
+<?php
+// Set CORS headers (same as login.php/register.php)
+header("Access-Control-Allow-Origin: http://smallproject.cjanua.xyz");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+$inData = getRequestInfo();
+
+// Use the same DB credentials as in login.php/register.php
+$dbUser = "root";
+$dbPass = "kVIuL:H/t4P8";
+$dbName = "ContactManager";
+
+$conn = new mysqli("localhost", $dbUser, $dbPass, $dbName);
+if ($conn->connect_error) {
+    returnWithError("Connection failed: " . $conn->connect_error);
+} else {
+    $stmt = $conn->prepare("UPDATE contacts SET FirstName=?, LastName=?, Email=?, PhoneNumber=? WHERE ID=? AND IDnum=?");
+    if (!$stmt) {
+        returnWithError("Prepare failed: " . $conn->error);
+    } else {
+        $stmt->bind_param("ssssii", $inData["FirstName"], $inData["LastName"], $inData["Email"], $inData["PhoneNumber"], $inData["ID"], $inData["IDnum"]);
+        if ($stmt->execute()) {
+            if ($stmt->affected_rows > 0) {
+                returnWithMessage("Contact updated successfully.");
+            } else {
+                returnWithError("Could not update contact.");
+            }
+        } else {
+            returnWithError("Error executing update: " . $stmt->error);
+        }
+        $stmt->close();
+    }
+    $conn->close();
+}
+
+function getRequestInfo() {
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj) {
+    header('Content-type: application/json');
+    echo json_encode($obj);
+}
+
+function returnWithError($err) {
+    $retValue = ["error" => $err];
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithMessage($message) {
+    $retValue = ["message" => $message];
+    sendResultInfoAsJson($retValue);
+}
+?>
