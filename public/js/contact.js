19349bd795e27aeeecfd10b240239a3b7d028d75
Christian Costa <gunnerhatfield1206@gmail.com>
2025-02-24 11:55:25 -0500

    debug changes

--- public/js/contact.js
+++ public/js/contact.js
@@ -391,7 +391,7 @@ function delay(ms) {
 window.onload = function () {
     if (window.location.pathname.endsWith('contacts.html')) {
         readCookie(); // Ensure `userId` is read
-	console.log("User ID: " + iduser); //DEBUG
+	console.log("User ID: " + userId); //DEBUG
         if (userId > 0) {
             loadContacts();
         }
